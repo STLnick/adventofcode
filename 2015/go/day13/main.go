0cb48a1e50c297eb6c865846798a9c8868f8e8af
Nick <nickrray@gmail.com>
2023-09-16 14:30:27 -0500

    Go: Day 13 Part 2

--- 2015/go/day13/main.go
+++ 2015/go/day13/main.go
@@ -18,19 +18,26 @@ type instruction struct {
 	p2     string
 }
 
+const SELF string = "SELF"
+
 func buildKeyAndList(instructions *[]instruction) (map[string]map[string]int16, []string) {
 	key := make(map[string]map[string]int16)
+	key[SELF] = make(map[string]int16)
 	var names []string
 
 	for _, ins := range *instructions {
 		if !slices.Contains(names, ins.p1) {
 			names = append(names, ins.p1)
 			key[ins.p1] = make(map[string]int16)
+			key[ins.p1][SELF] = 0
+			key[SELF][ins.p1] = 0
 		}
 
 		if !slices.Contains(names, ins.p2) {
 			names = append(names, ins.p2)
 			key[ins.p2] = make(map[string]int16)
+			key[ins.p2][SELF] = 0
+			key[SELF][ins.p2] = 0
 		}
 
 		amt := ins.amt
@@ -45,7 +52,7 @@ func buildKeyAndList(instructions *[]instruction) (map[string]map[string]int16, 
 }
 
 func getInstructions(fileName string) []instruction {
-    directory, err := os.Getwd()
+	directory, err := os.Getwd()
 	if err != nil {
 		log.Fatal("error reading working directory", err)
 	}
@@ -98,60 +105,112 @@ func getInstructions(fileName string) []instruction {
 }
 
 func removeName(s []string, n string) []string {
-    newSlice := copySlice(s)
+	newSlice := copySlice(s)
 	i := slices.Index(newSlice, n)
-    if i != -1 {
-	    return slices.Replace(newSlice, i, i+1)
-    }
-    return newSlice
+	if i != -1 {
+		return slices.Replace(newSlice, i, i+1)
+	}
+	return newSlice
 }
 
 func copySlice(s []string) []string {
-    newSlice := make([]string, len(s), cap(s))
-    copy(newSlice, s)
-    return newSlice
+	newSlice := make([]string, len(s), cap(s))
+	copy(newSlice, s)
+	return newSlice
 }
 
 func calculateHappiness(peopleKey map[string]map[string]int16, names []string) int16 {
-    var total int16
+	var total int16
 
-    for i, name := range names {
-        prevIndex := i-1
-        if prevIndex < 0 {
-            prevIndex = len(names) - 1
-        }
-        total += peopleKey[name][names[prevIndex]]
+	for i, name := range names {
+		prevIndex := i - 1
+		if prevIndex < 0 {
+			prevIndex = len(names) - 1
+		}
+		total += peopleKey[name][names[prevIndex]]
 
-        nextIndex := i+1
-        if nextIndex == len(names) {
-            nextIndex = 0
-        }
-        total += peopleKey[name][names[nextIndex]]
-    }
+		nextIndex := i + 1
+		if nextIndex == len(names) {
+			nextIndex = 0
+		}
+		total += peopleKey[name][names[nextIndex]]
+	}
 
-    return total
+	return total
 }
 
 func compileCombination(c chan []string, seated []string, remaining []string, firstSeating string) {
-    newSeated := append(copySlice(seated), firstSeating)
-    newRemaining := removeName(remaining, firstSeating)
+	newSeated := append(copySlice(seated), firstSeating)
+	newRemaining := removeName(remaining, firstSeating)
 
-    if len(newRemaining) > 0 {
-        lwg := sync.WaitGroup{}
-        for _, rn := range newRemaining {
-            lwg.Add(1)
-            r := copySlice(newRemaining)
-            s := copySlice(newSeated)
+	if len(newRemaining) > 0 {
+		lwg := sync.WaitGroup{}
+		for _, rn := range newRemaining {
+			lwg.Add(1)
+			r := copySlice(newRemaining)
+			s := copySlice(newSeated)
 
-            go func(name string) {
-                defer lwg.Done()
-                compileCombination(c, s, r, name)
-            }(rn)
-        }
-        lwg.Wait()
-    } else {
-        c <- newSeated
-    }
+			go func(name string) {
+				defer lwg.Done()
+				compileCombination(c, s, r, name)
+			}(rn)
+		}
+		lwg.Wait()
+	} else {
+		c <- newSeated
+	}
+}
+
+func getPossibleInts(
+	key map[string]map[string]int16,
+	names []string,
+	withSelf bool,
+) ([]int16, int16) {
+	var vals []int16
+
+	if withSelf {
+		names = append(names, SELF)
+	}
+
+	wg := &sync.WaitGroup{}
+	resultChan := make(chan []string)
+	remaining := copySlice(names)
+
+	for _, n := range names {
+		wg.Add(1)
+		r := copySlice(remaining)
+
+		go func(name string) {
+			defer wg.Done()
+			compileCombination(
+				resultChan,
+				make([]string, 0, len(names)),
+				r,
+				name,
+			)
+		}(n)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	var possibilities [][]string
+	var highest int16
+	var tempVal int16
+
+	for list := range resultChan {
+		possibilities = append(possibilities, list)
+		tempVal = calculateHappiness(key, list)
+
+		if tempVal > highest {
+			highest = tempVal
+		}
+		vals = append(vals, tempVal)
+	}
+
+	return vals, highest
 }
 
 func main() {
@@ -160,49 +219,18 @@ func main() {
 	input := flag.String("input", "input.txt", "The file to read as input to program")
 	flag.Parse()
 
-	wg := &sync.WaitGroup{}
-	resultChanStr := make(chan []string)
 	instructions := getInstructions(*input)
 	peopleKey, names := buildKeyAndList(&instructions)
-    remaining := copySlice(names)
 
-	for _, n := range names {
-		wg.Add(1)
-        r := copySlice(remaining)
-		
-        go func(name string) {
-            defer wg.Done()
-            compileCombination(
-                resultChanStr,
-                make([]string, 0, len(names)),
-                r,
-                name,
-            )
-        }(n)
-	}
+	fmt.Println("*** Part 1 *** :: BEGIN")
+	partOneVals, highestOne := getPossibleInts(peopleKey, names, false)
+	fmt.Println("Part One Vals len: ", len(partOneVals))
+	fmt.Printf("Highest value (%d)\n", highestOne)
+	fmt.Println("*** Part 1 *** :: END\n")
 
-	go func() {
-		wg.Wait()
-		close(resultChanStr)
-	}()
-
-    var possibilities [][]string
-    var intVals []int16
-    var highest int16
-    var tempVal int16
-
-	for val := range resultChanStr {
-        possibilities = append(possibilities, val)
-        tempVal = calculateHappiness(peopleKey, val)
-
-        if tempVal > highest {
-            highest = tempVal
-        }
-        intVals = append(intVals, tempVal)
-	}
-
-    // TODO: Check and ensure `possibilities` is all unique lists
-
-    fmt.Printf("Received %d Values from Channel\n", len(intVals))
-    fmt.Printf("Highest value (%d)\n", highest)
+	fmt.Println("*** Part 2 *** :: START")
+	partTwoVals, highestTwo := getPossibleInts(peopleKey, names, true)
+	fmt.Println("Part Two Vals len: ", len(partTwoVals))
+	fmt.Printf("Highest value (%d)\n\n", highestTwo)
+	fmt.Println("*** Part 2 *** :: END")
 }
