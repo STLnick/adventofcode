30c522b74c43b6741da938b4b11b4fabd2da5a38
Nick <nickrray@gmail.com>
2023-09-30 20:37:05 -0500

    Go: Day 19 Part 2 - That was painful

--- 2015/go/day19/main.go
+++ 2015/go/day19/main.go
@@ -4,14 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 	"slices"
 	"strings"
+	"unicode"
 )
 
 type transform struct {
 	from string
 	to   string
+}
+
+func (t transform) String() string {
+	return fmt.Sprintf("%s => %s", t.from, t.to)
 }
 
 func checkFatal(err error, msg string) {
@@ -22,11 +28,19 @@ func checkFatal(err error, msg string) {
 
 func setup() ([]transform, string) {
 	fileName := "input.txt"
+	p2 := flag.Bool("p2", false, "use input2 for logic")
 	useTest := flag.Bool("test", false, "use test file for input")
+	useTest2 := flag.Bool("test2", false, "use test2 file for input")
 	flag.Parse()
 
 	if *useTest {
 		fileName = "test.txt"
+	} else if *useTest2 {
+		fileName = "test2.txt"
+		isPartTwo = true
+	} else if *p2 {
+		fileName = "input2.txt"
+		isPartTwo = true
 	}
 
 	fmt.Println("File Name:", fileName)
@@ -54,7 +68,7 @@ func setup() ([]transform, string) {
 	return transforms, lines[startIdx]
 }
 
-func performTransforms(s string, t transform, start int) string {
+func performTransformsRecursive(s string, t transform, start int) string {
 	if start >= len(s) {
 		return s
 	}
@@ -75,23 +89,79 @@ func performTransforms(s string, t transform, start int) string {
 	}
 
 	newStr += cloned[tailStart:]
-	return newStr + "::" + performTransforms(s, t, idx+1)
+	return newStr + "::" + performTransformsRecursive(s, t, idx+1)
 }
+
+var isPartTwo = false
+var logger = slog.Default()
 
 func main() {
 	fmt.Println("-- Day 19 --")
 
 	transforms, start := setup()
-	var strs []string
 
-	for _, t := range transforms {
-		joinedStr := performTransforms(start, t, 0)
-		for _, s := range strings.Split(joinedStr, "::") {
-			if !slices.Contains(strs, s) && s != start {
-				strs = append(strs, s)
+	if !isPartTwo {
+		var strs []string
+
+		for _, t := range transforms {
+			joinedStr := performTransformsRecursive(start, t, 0)
+			for _, s := range strings.Split(joinedStr, "::") {
+				if !slices.Contains(strs, s) && s != start {
+					strs = append(strs, s)
+				}
 			}
 		}
-	}
 
-	fmt.Println("main() :: # of strs", len(strs))
+		fmt.Println("main() :: PART ONE :: # of strs", len(strs))
+	} else {
+		input := "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
+		var (
+			tokens   []string
+			commaCt  int
+			parensCt int
+			c        rune
+		)
+
+		fmt.Printf("Tokens initialized with len (%d) (%v)\n", len(tokens), tokens)
+
+		temp := string(input[0])
+
+		for i := 1; i < len(input); i++ {
+			c = rune(input[i])
+
+			if unicode.IsUpper(c) {
+				tokens = append(tokens, temp)
+
+				if i == 0 || i == 1 {
+					fmt.Printf("Tokens initialized with len (%d) (%v)\n", len(tokens), tokens)
+				}
+
+				if temp == "Y" {
+					commaCt++
+				} else if temp == "Rn" || temp == "Ar" {
+					parensCt++
+				}
+				temp = string(c)
+			} else {
+				temp += string(c)
+				if i == len(input)-1 {
+					tokens = append(tokens, temp)
+					if temp == "Y" {
+						commaCt++
+					} else if temp == "Rn" || temp == "Ar" {
+						parensCt++
+					}
+				}
+			}
+		}
+
+		fmt.Printf("%-8s%5d\n", "Tokens", len(tokens))
+		fmt.Printf("%-8s%5d\n", "commaCt", commaCt)
+		fmt.Printf("%-8s%5d\n", "parensCt", parensCt)
+
+		// count(tokens) - count(parens) - 2*count("Y") - 1
+		answer := len(tokens) - parensCt - (2 * commaCt) - 1
+
+		fmt.Printf("%-8s%5d\n", "Answer", answer)
+	}
 }
