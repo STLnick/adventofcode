5a862f96f77d88f7c4d2ff1b9662c50ade09514a
STLnick <nickrray@gmail.com>
2023-12-07 08:12:47 -0600

    Go: Day4 part 2

--- 2023/go/day4/main.go
+++ 2023/go/day4/main.go
@@ -16,15 +16,20 @@ func handleError(e error) {
 	}
 }
 
-func checkCard(cardStr string) int {
-	var points int
+func parseCardNumbers(cardStr string) ([]string, []string) {
 	cardParts := strings.Split(cardStr, ": ")
 	numberParts := strings.Split(cardParts[1], " | ")
 	winningNums := strings.Fields(numberParts[0])
 	cardNums := strings.Fields(numberParts[1])
 
+	return winningNums, cardNums
+}
+
+func checkCardPoints(cardStr string) int {
+	var points int
+	winningNums, cardNums := parseCardNumbers(cardStr)
+
 	for _, cardNum := range cardNums {
-		//fmt.Println("cardNum:", cardNum)
 		if slices.Contains(winningNums, cardNum) {
 			if points == 0 {
 				points = 1
@@ -35,6 +40,26 @@ func checkCard(cardStr string) int {
 	}
 
 	return points
+}
+
+func checkCardMatches(cardStr string) int {
+	var matches int
+	winningNums, cardNums := parseCardNumbers(cardStr)
+
+	for _, cardNum := range cardNums {
+		if slices.Contains(winningNums, cardNum) {
+			matches += 1
+		}
+	}
+
+	return matches
+}
+
+func printMap(lineMap map[int]int) {
+	//for k, v := range lineMap {
+	for i := 0; i < len(lineMap); i++ {
+		fmt.Printf("Card %d :: number of cards %d\n", i, lineMap[i])
+	}
 }
 
 func main() {
@@ -53,7 +78,7 @@ func main() {
 
 	file, err := os.Open(dir + "/" + filename)
 	handleError(err)
-	defer file.Close()
+	//defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	var (
@@ -62,13 +87,54 @@ func main() {
 	)
 
 	for scanner.Scan() {
-		cardPoints = checkCard(scanner.Text())
-		//fmt.Println("cardPoints", cardPoints)
+		cardPoints = checkCardPoints(scanner.Text())
 		pointSum += cardPoints
 	}
-	fmt.Println("pointSum", pointSum)
+
+	fmt.Println("Part 1: pointSum", pointSum)
 
 	if err := scanner.Err(); err != nil {
 		handleError(err)
 	}
+	err = file.Close()
+	handleError(err)
+
+	// Part 2
+	var cardMatches int
+	part2File, err := os.ReadFile(dir + "/" + filename)
+	lines := strings.Split(string(part2File), "\n")
+	lines = lines[:len(lines)-1]
+
+	lineMap := make(map[int]int)
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+		lineMap[i] = 1
+	}
+
+	for i := 0; i < len(lines); i++ {
+		cardMatches = checkCardMatches(lines[i])
+		if cardMatches == 0 {
+			continue
+		}
+
+		cardsToPlay := lineMap[i]
+		for j := 0; j < cardsToPlay; j++ {
+			localMatches := cardMatches
+			for idx := i + 1; idx < len(lines) && localMatches > 0; idx++ {
+				lineMap[idx] += 1
+				localMatches -= 1
+			}
+		}
+	}
+
+	printMap(lineMap)
+
+	var cardCount int
+	for i := 0; i < len(lineMap); i++ {
+		cardCount += lineMap[i]
+	}
+
+	fmt.Println("Part 2: cardCount", cardCount)
 }
