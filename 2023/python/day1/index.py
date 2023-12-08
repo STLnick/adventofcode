6d99995fe0b6a5781b3f9591d8672f7a27d54f17
STLnick <nickrray@gmail.com>
2023-12-07 20:06:29 -0600

    Python: Day 1 part 2

--- 2023/python/day1/index.py
+++ 2023/python/day1/index.py
@@ -1,8 +1,17 @@
 print("-- Day 1 --")
 
-# Part one - find first and last digit, combine to make 2 digit num, sum all numbers
+numMap = {"one": 1,
+          "two": 2,
+          "three": 3,
+          "four": 4,
+          "five": 5,
+          "six": 6,
+          "seven": 7,
+          "eight": 8,
+          "nine": 9}
 
 #file = open("input-test.txt", "r")
+#file = open("input-testp2.txt", "r")
 file = open("input.txt", "r")
 
 lines = file.readlines()
@@ -10,20 +19,39 @@ lines = file.readlines()
 sum = 0
 result = -1
 for line in lines:
-    start = -1
-    end = -1
+    first = -1
+    firstIdx = -1
+    last = -1
+    lastIdx = -1
 
-    for char in line:
+    # Find first and last digits in line, record positions and values
+    for cIdx, char in enumerate(line):
         if char.isdigit():
-            if start == -1:
-                start = int(char)
-                end = int(char)
+            if first == -1:
+                first = int(char)
+                firstIdx = cIdx
+                last = int(char)
+                lastIdx = cIdx
             else:
-                end = int(char)
+                last = int(char)
+                lastIdx = cIdx
 
-    result = (start * 10) + end
-    print("result -", result)
+    # Find first and last NUMBER strings in line, record positions and values
+    for _, numStr in enumerate(numMap):
+        numIdx = 0
+        while numIdx != -1:
+            numIdx = line.find(numStr, numIdx if numIdx == 0 else numIdx + 1)
+
+            if numIdx != -1:
+                if numIdx < firstIdx or firstIdx == -1:
+                    firstIdx = numIdx
+                    first = numMap[numStr]
+                if numIdx > lastIdx or lastIdx == -1:
+                    lastIdx = numIdx
+                    last = numMap[numStr]
+                numIdx += len(numStr) - 1
+
+    result = (first * 10) + last
     sum += result
 
 print("Sum:", sum)
-
