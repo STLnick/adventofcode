104456b0d8e2bcc099f8ff3ee920c91fb5db94bc
Nick <nickrray@gmail.com>
2023-12-20 23:48:03 -0600

    JS: Day 6 part 2 create much faster solution

--- 2023/javascript/day6/index.js
+++ 2023/javascript/day6/index.js
@@ -34,28 +34,7 @@ function getMMTraveled(timeMs, msHeld) {
   return (timeMs - msHeld) * msHeld;
 }
 
-function part1() {
-  const tdMap = getTimeDistanceMap();
-  const times = Object.keys(tdMap);
-  let possibilities = {};
-
-  times.forEach(timeMs => {
-    possibilities[timeMs] = 0;
-    
-    for (let held = 1; held < timeMs; held++) {
-      if (getMMTraveled(timeMs, held) > tdMap[timeMs]) {
-        possibilities[timeMs]++;
-      }
-    }
-  });
-  
-  console.log(possibilities);
-  const product = Object.values(possibilities).reduce((acc, num) => acc * num, 1);
-  console.log(product);
-}
-
-function part2() {
-  const tdMap = getTimeDistanceMap(true);
+function getPossibilities(tdMap) {
   const times = Object.keys(tdMap);
   let possibilities = {};
   let reachedPossibles;
@@ -68,17 +47,79 @@ function part2() {
       if (getMMTraveled(timeMs, held) > tdMap[timeMs]) {
         if (!reachedPossibles) reachedPossibles = true
         possibilities[timeMs]++;
-      } else if (reachedPossibles && getMMTraveled(timeMs, held) <= tdMap[timeMs]) {
+      } else if (reachedPossibles) {
         break;
       }
     }
   });
+
+  return possibilities;
+}
+
+function part1() {
+  const tdMap = getTimeDistanceMap();
   
+  let possibilities = getPossibilities(tdMap);
+  console.log("P1:", possibilities);
+  
+  const product = Object.values(possibilities).reduce((acc, num) => acc * num, 1);
+  console.log("P1:", product);
+}
+
+function part2() {
+  const tdMap = getTimeDistanceMap(true);
+  
+  let possibilities = getPossibilities(tdMap);
   console.log("P2", possibilities);
+  
   const product = Object.values(possibilities).reduce((acc, num) => acc * num, 1);
   console.log("P2", product);
 }
 
+function part2_1() {
+  const tdMap = getTimeDistanceMap(true);
+  const timeMs = Object.keys(tdMap)[0];
+  const recordDist = tdMap[timeMs];
+  const msHeld = Math.floor(+timeMs / 2);
+  const root = Math.floor(Math.sqrt(+timeMs));
+  let start = msHeld;
+  let end = msHeld;
+
+  //work begin leftwards until edge
+  while (start > 0 && getMMTraveled(+timeMs, start) > recordDist) {
+    start -= root;
+  }
+  start += root;
+
+  while (start > 0 && getMMTraveled(+timeMs, start) > recordDist) {
+    start -= 1;
+  }
+  start += 1;
+  
+  //work end rightwards until edge
+  while (end < timeMs && getMMTraveled(+timeMs, end) > recordDist) {
+    end += root;
+  }
+  end -= root;
+
+  while (end < timeMs && getMMTraveled(+timeMs, end) > recordDist) {
+    end += 1;
+  }
+  end -= 1;
+
+  const possiblities = end - start + 1;
+  console.log("P2_1", possiblities);
+}
+
+console.time("p1")
 part1();
+console.timeEnd("p1")
+
+console.time("p2")
 part2();
+console.timeEnd("p2")
+
+console.time("p2_1")
+part2_1();
+console.timeEnd("p2_1")
 
