ed4adf5188d692ed3765494f97a75481834a0aee
STLnick <nickrray@gmail.com>
2023-12-23 11:43:25 -0600

    JS: Day 9 part 2

--- 2023/javascript/day9/index.js
+++ 2023/javascript/day9/index.js
@@ -30,8 +30,7 @@ function getFutureValue(history) {
   return futureVal;
 }
 
-function part1() {
-  const historyList = getLines().map(s => s.split(" ").map(n => parseInt(n)));
+function part1(historyList) {
   let futureValues = new Array(historyList.length).fill(null);
   let history;
   
@@ -43,9 +42,55 @@ function part1() {
   return futureValues.reduce((sum, v) => sum + v, 0);
 }
 
+function getPastValue(history) {
+  let workingSet = [ ...history ];
+  let newSet;
+  let firstVals = [ workingSet[0] ];
+  let current, next;
+
+  while (!workingSet.every(v => v === 0)) {
+    newSet = [];
+
+    for (let i = 0; i < workingSet.length - 1; i++) {
+      current = workingSet[i];
+      next = workingSet[i + 1];
+      newSet.push(next - current);
+    }
+
+    workingSet = [ ...newSet ];
+    firstVals.unshift(workingSet[0]);
+  }
+
+  let nextVal;
+  let pastVal = firstVals[0];
+  for (let i = 1; i < firstVals.length; i++) {
+    nextVal = firstVals[i];
+    pastVal = nextVal - pastVal;
+  }
+
+  return pastVal;
+
+}
+
+function part2(historyList) {
+  let pastValues = new Array(historyList.length).fill(null);
+  let history;
+  
+  for (let i = 0; i < historyList.length; i++) {
+    history = historyList[i];
+    pastValues[i] = getPastValue(history);
+  }
+
+  return pastValues.reduce((sum, v) => sum + v, 0);
+}
+
 function main() {
-  const p1Result = part1();
+  const historyList = getLines().map(s => s.split(" ").map(n => parseInt(n)));
+
+  const p1Result = part1(historyList);
   console.log("P1 Result - future values sum:", p1Result);
+  const p2Result = part2(historyList);
+  console.log("P2 Result - past values sum:", p2Result);
 }
 
 main();
