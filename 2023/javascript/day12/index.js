3e5ffe024c3588329ea1294d2f1218c0cd7c9032
Nick <nickrray@gmail.com>
2023-12-26 11:38:05 -0600

    JS: Day 12 part 1 WIP

--- 2023/javascript/day12/index.js
+++ 2023/javascript/day12/index.js
@@ -21,19 +21,74 @@ async function* getLine() {
   yield null;
 }
 
-async function part1() {
-  let groups, springs;
+function checkhighestOfGroups(groups, num) {
+  const highest = groups.reduce((hi, n) => hi < n ? n : hi, 0);
+  return highest === num;
+}
 
+function processSpringsStr(springs, groups) {
+  console.log("\n- - - - - - - - - - - - - - - - - - - - -");
+  console.log("processSpringsStr() :: ", { springs: springs.join(""), groups });
+
+  let checkIdx, end, group, start;
+  let nextStart = 0;
+  let prevEnd = 0;
+  let possible = false;
+
+  for (let g = 0; g < groups.length; g++) {
+    group = parseInt(groups[g]);
+    start = parseInt(nextStart);
+    end = start + group;
+    possible = false;
+    checkIdx = 0;
+
+    // start to walk string to find a possibility
+    // find a X length str of "#" or "?" or a combo
+    console.log("processSpringsStr() :: group", group, ":", { start, end });
+    
+    while (!possible) {
+      possible = true;
+
+      for (checkIdx = start; checkIdx < end; checkIdx++) {
+        if (springs[checkIdx] === springStates.OPERATIONAL) {
+          possible = false;
+          break;
+        }
+      }
+   
+      if (!possible) {
+        start++;
+        end++;
+      }
+    }
+
+    console.log("* Possible section for(", group, "):(", start, ",", end, "):", springs.slice(start, end));
+
+    prevEnd = end;
+    nextStart = end + 1;
+  }
+}
+
+async function part1() {
+  let groups, springsStr, springs;
+
+  let logged = false;
   for await (const line of getLine()) {
     if (line === null) {
       break;
     }
 
-    [ springs, groups ] = line.split(" ");
-    springs = springs.split("");
+    [ springsStr, groups ] = line.split(" ");
+    springs = springsStr.split("");
     groups = groups.split(",");
 
-    /**LOG*/ console.log({ line, springs, groups });
+    // processSpringsStr(springs, groups);
+    if (!logged) {
+      processSpringsStr(springs, groups);
+      logged = true;
+    }
+
+    // /**LOG*/ console.log({ line, springs, groups });
   }
 }
 
